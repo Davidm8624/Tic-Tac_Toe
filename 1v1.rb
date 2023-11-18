3f20f52a03b032a170bc83f4c8a0d9908841a5e3
Davidm8624 <davidm8624@gmail.com>
2023-11-18 15:16:10 -0700

    dunno

---
+++ 1v1.rb
@@ -0,0 +1,18 @@
+class GameBoard
+# winning combinations (array)
+
+#initilization (creation of the board)
+
+#display the board after each move
+
+#check for winner based on the winning combinations array
+
+#make move (position) player should already be selected without user input
+
+#starting the game & each turn
+
+#tell who won based off of checkforwinner method above
+end
+
+game = GameBoard.new
+game.start_game
